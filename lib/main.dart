53eca8cefeba2967147f513c9b7987c1bcc910eb
Paul Betts <paul@paulbetts.org>
2019-04-08 08:08:37 -0700

    Publish v0.0.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:catcher/catcher_plugin.dart';
+import 'package:flutter/material.dart';
 import 'package:sirene/services/logging.dart';
 
 import './app.dart';
 
 final config = CatcherOptions(SilentReportMode(), [LoggingCatcherHandler()]);
-void main() => Catcher(AppWidget(), debugConfig: config, releaseConfig: config);
+//void main() => Catcher(AppWidget(), debugConfig: config, releaseConfig: config);
+void main() => runApp(AppWidget());
