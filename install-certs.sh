2a2616bf244b42fc3cd1c4b62d28b92c222bf45a
Paul Betts <paul@paulbetts.org>
2019-07-01 23:31:22 -0700

    #yolo

--- install-certs.sh
+++ install-certs.sh
@@ -4,5 +4,5 @@ security create-keychain -p "$CERT_PASSWORD" dontcare.keychain
 security default-keychain -s dontcare.keychain
 security unlock-keychain -p "$CERT_PASSWORD" dontcare.keychain
 
-security import $TEMPDIR/developer.p12 -k dontcare.keychain -P "$CERT_PASSWORD" -T /usr/bin/codesign
-security import $TEMPDIR/distribution.p12 -k dontcare.keychain -P "$CERT_PASSWORD" -T /usr/bin/codesign
+security import $TEMPDIR/developer.p12 -k dontcare.keychain -P "$CERT_PASSWORD" -A
+security import $TEMPDIR/distribution.p12 -k dontcare.keychain -P "$CERT_PASSWORD" -A
