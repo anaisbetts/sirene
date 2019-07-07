deb45f893a3daf7ea4ebfc8cfa1d69de439cb175
Anaïs Betts <anais@anaisbetts.org>
2019-07-06 21:39:59 -0700

    More Magic Words

--- install-certs.sh
+++ install-certs.sh
@@ -10,6 +10,9 @@ security unlock-keychain -p "$CERT_PASSWORD" "$KEYCHAIN_FILE"
 security import $TEMPDIR/developer.p12 -k "$KEYCHAIN_FILE" -P "$CERT_PASSWORD" -A
 security import $TEMPDIR/distribution.p12 -k "$KEYCHAIN_FILE" -P "$CERT_PASSWORD" -A
 
+security set-keychain-settings -t 3600 -u "$KEYCHAIN_FILE"
+security list-keychains -s "$KEYCHAIN_FILE"
+
 ## NB: We need this for some reason, or else macOS (despite us already unlocking the keychain),
 ## will open a modal dialog and park the builder forever
 security set-key-partition-list -S apple-tool:,apple: -s -k "$CERT_PASSWORD" "$KEYCHAIN_FILE"
