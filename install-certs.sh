c7969e807d2e71fb6cf17d67f5b40991544a4068
Anaïs Betts <anais@anaisbetts.org>
2019-07-06 20:22:38 -0700

    :fire: some debugging code

--- install-certs.sh
+++ install-certs.sh
@@ -10,6 +10,6 @@ security unlock-keychain -p "$CERT_PASSWORD" "$KEYCHAIN_FILE"
 security import $TEMPDIR/developer.p12 -k "$KEYCHAIN_FILE" -P "$CERT_PASSWORD" -A
 security import $TEMPDIR/distribution.p12 -k "$KEYCHAIN_FILE" -P "$CERT_PASSWORD" -A
 
-
-security find-identity -v -p codesigning "$KEYCHAIN_FILE"
+## NB: We need this for some reason, or else macOS (despite us already unlocking the keychain),
+## will open a modal dialog and park the builder forever
 security set-key-partition-list -S apple-tool:,apple: -s -k "$CERT_PASSWORD" "$KEYCHAIN_FILE"
