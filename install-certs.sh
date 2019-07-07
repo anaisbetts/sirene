4c956aaed9e350bf83a23fcf8a700c0cbd1c3d66
Anaïs Betts <anais@anaisbetts.org>
2019-07-06 19:59:47 -0700

    Let's see if this fixes our hang

--- install-certs.sh
+++ install-certs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -euo pipefail
 
-security create-keychain -p "$CERT_PASSWORD" dontcare.keychain
+KEYCHAIN_FILE="$TEMPDIR/dontcare.keychain"
+
+security create-keychain -p "$CERT_PASSWORD" "$KEYCHAIN_FILE"
 security default-keychain -s dontcare.keychain
-security unlock-keychain -p "$CERT_PASSWORD" dontcare.keychain
+security unlock-keychain -p "$CERT_PASSWORD" "$KEYCHAIN_FILE"
+security set-key-partition-list -S apple-tool:,apple: -s -k "$CERT_PASSWORD" "$KEYCHAIN_FILE"
 
-security import $TEMPDIR/developer.p12 -k dontcare.keychain -P "$CERT_PASSWORD" -A
-security import $TEMPDIR/distribution.p12 -k dontcare.keychain -P "$CERT_PASSWORD" -A
+security import $TEMPDIR/developer.p12 -k "$KEYCHAIN_FILE" -P "$CERT_PASSWORD" -A
+security import $TEMPDIR/distribution.p12 -k "$KEYCHAIN_FILE" -P "$CERT_PASSWORD" -A
