38b7c2d7c6e861bfa8b6a3e1b7acebaf434a31c5
Anaïs Betts <anais@anaisbetts.org>
2019-07-06 20:14:27 -0700

    I am an idiot.

--- install-certs.sh
+++ install-certs.sh
@@ -7,9 +7,9 @@ security create-keychain -p "$CERT_PASSWORD" "$KEYCHAIN_FILE"
 security default-keychain -s "$KEYCHAIN_FILE"
 security unlock-keychain -p "$CERT_PASSWORD" "$KEYCHAIN_FILE"
 
-security find-identity -v -p codesigning "$KEYCHAIN_FILE"
-
-security set-key-partition-list -S apple-tool:,apple: -s -k "$CERT_PASSWORD" "$KEYCHAIN_FILE"
-
 security import $TEMPDIR/developer.p12 -k "$KEYCHAIN_FILE" -P "$CERT_PASSWORD" -A
 security import $TEMPDIR/distribution.p12 -k "$KEYCHAIN_FILE" -P "$CERT_PASSWORD" -A
+
+
+security find-identity -v -p codesigning "$KEYCHAIN_FILE"
+security set-key-partition-list -S apple-tool:,apple: -s -k "$CERT_PASSWORD" "$KEYCHAIN_FILE"
